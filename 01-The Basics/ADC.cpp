09239f142538305f18cb1d7972ca11796b13c871
ftsipis <154264822+ftsipis@users.noreply.github.com>
2026-07-28 12:57:10 +0300

    01- fix

--- 01-The Basics/ADC.cpp
+++ 01-The Basics/ADC.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int adc()           // generate a random int from 0 to 4095
 {
-    random_device dev;
-    mt19937 rng(dev());
-    uniform_int_distribution<mt19937::result_type> dist(0,4095); // distribution in range [1, 6]
+    static random_device dev;
+    static mt19937 rng(dev());
+    static uniform_int_distribution<mt19937::result_type> dist(0,4095); // distribution in range [1, 6]
 
     return dist(rng);
 }
