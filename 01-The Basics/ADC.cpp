daec8e57059a0ec7e2304c86335c1229381777ba
ftsipis <154264822+ftsipis@users.noreply.github.com>
2026-07-31 19:54:11 +0300

    01-fix

--- 01-The Basics/ADC.cpp
+++ 01-The Basics/ADC.cpp
@@ -8,7 +8,8 @@ constexpr int adc_max_counts = 4095;    // 2^12 - 1
 
 int adc_read()
 {
-    static mt19937 rng(random_device{});
+    static random_device dev;
+    static mt19937 rng(dev());
     static uniform_int_distribution<int> dist(0,adc_max_counts);
     return dist(rng);
 }
