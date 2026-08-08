7af0357202e7384e1646721520564b19b34f5a6d
ftsipis <154264822+ftsipis@users.noreply.github.com>
2026-08-08 03:06:59 +0300

    01

--- 01-The Basics/adc_solution.cpp
+++ 01-The Basics/adc_solution.cpp
@@ -17,7 +17,7 @@ int adc_read()
 
 void print_buffer(int buffer[])
 {
-    for (int x=0; x<buffer_size; x++)
+    for (int x=0; x<buffer_size-1; x++)
     {
         cout << buffer[x] << '\n';
     }
@@ -25,7 +25,12 @@ void print_buffer(int buffer[])
 
 void minimum (int buffer[])
 {
-    cout << min(buffer) << "\n\n";
+    int min;
+    for (int x=0; x<buffer_size; x++)
+    {
+        min(buffer_size[x],buffer[x+1]);
+    }
+    return min;
 }
 
 int main()
