ab19c18a9a1e4c795d7674ab68e8b8f7088a39d6
ftsipis <154264822+ftsipis@users.noreply.github.com>
2026-08-10 10:16:36 +0300

    01

--- 01-The Basics/adc_solution.cpp
+++ 01-The Basics/adc_solution.cpp
@@ -71,7 +71,7 @@ void clamp (int buffer[])
 void reverse (int buffer[])
 {
     int* n = buffer;
-    int* p = buffer +  - 1;
+    int* p = buffer + buffer_size - 1;
     int temp;
     //while (n < p)
     
